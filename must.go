6c0c82b2fab9daf7c628013dfac68f44fb17e311
Andrew Jackson (Ajax) <snadrus@gmail.com>
2024-06-17 16:28:19 -0500

    Many and friends

--- must.go
+++ must.go
@@ -3,6 +3,7 @@ package must
 import (
 	"io"
 
+	"golang.org/x/sync/errgroup"
 	"golang.org/x/xerrors"
 )
 
@@ -49,4 +50,39 @@ func RecoverToErr(retErr *error) {
 func With[C io.Closer](c C, f func(C)) {
 	defer c.Close()
 	f(c)
+}
+
+// Many starts parallel goroutines that handle errors through panic forwarding.
+func Many(fs ...func()) {
+	g := errgroup.Group{}
+	for a := range fs {
+		g.Go(func() (err error) {
+			defer func() {
+				if r := recover(); r != nil {
+					err = xerrors.Errorf("%w", r)
+				}
+			}()
+			fs[a]()
+			return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		panic(err)
+	}
+}
+
+// ErrOnly is a helper function to return an error if it is not nil.
+// This is good for Many() when you don't need the first argument.
+// Usage:
+//
+//	must.Many(
+//	 must.ErrOnly2(db.Exec("INSERT INTO foo VALUES (?)", 42))
+//	 must.ErrOnly2(db.Exec("INSERT INTO foo VALUES (?)", 43))
+//	)
+func ErrOnly2[R any](r R, err error) error {
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
